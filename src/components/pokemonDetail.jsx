6997dcd31319aeec863894a8c8c5211214995c6f
Mariano <marianotoma1234@gmail.com>
2026-05-29 21:24:54 +0200

    Pokedex final

--- src/components/pokemonDetail.jsx
+++ src/components/pokemonDetail.jsx
@@ -3,6 +3,28 @@ import { useParams } from "react-router";
 import axios from "axios";
 import "./pokemonDetail.css";
 
+// Colores de tipo oficiales
+const typeColors = {
+  grass: '#78C850',
+  fire: '#F08030',
+  water: '#6890F0',
+  bug: '#A8B820',
+  normal: '#A8A878',
+  electric: '#F8D030',
+  ground: '#E0C068',
+  fairy: '#EE99AC',
+  fighting: '#C03028',
+  psychic: '#F85888',
+  rock: '#B8A038',
+  ghost: '#705898',
+  ice: '#98D8D8',
+  dragon: '#7038F8',
+  dark: '#705848',
+  steel: '#B8B8D0',
+  flying: '#A890F0',
+  poison: '#A040A0',
+};
+
 export default function PokemonDetail() {
   const { name } = useParams();
   const [pokemon, setPokemon] = useState(null);
@@ -13,22 +35,35 @@ export default function PokemonDetail() {
         const response = await axios.get(
           `https://pokeapi.co/api/v2/pokemon/${name}`
         );
-
         setPokemon(response.data);
-
       } catch (error) {
         console.error(error);
       }
     };
-
     loadPokemon();
   }, [name]);
 
   if (!pokemon) return <p>Cargando...</p>;
 
+  // Degradado de fondo basado en los tipos
+  const pokemonTypes = pokemon.types.map((type) => type.type.name);
+  const color1 = typeColors[pokemonTypes[0]] || '#333';
+  const color2 = pokemonTypes[1] ? typeColors[pokemonTypes[1]] : color1;
+  
+  // Caso especial para el tipo 'fire' para imitar degradado naranja-amarillo
+  let gradientString = '';
+  if (pokemonTypes.length === 1 && pokemonTypes[0] === 'fire') {
+    gradientString = `linear-gradient(to bottom right, #F08030, #F8D030)`;
+  } else {
+    gradientString = `linear-gradient(to bottom right, ${color1}, ${color2})`;
+  }
+
   return (
-    <div className="pokemonDetail">
-        {/* Boton de retroceso a la pokedex */}
+    <div 
+      className="pokemonDetail"
+      style={{ backgroundImage: gradientString }}
+    >
+        {/* Botón de retroceso */}
         <button
             className="buttonBack"
             onClick={() => {
@@ -37,40 +72,42 @@ export default function PokemonDetail() {
           >
             ← 
         </button>
-        <h1>
-            {pokemon.name}
-        </h1>
 
+        <h1>{pokemon.name}</h1>
+
+        {/* Imagen principal del Pokémon */}
         <img
-            src={
-            pokemon.sprites.other["official-artwork"]
-                .front_default
-            }
+            src={pokemon.sprites.other["official-artwork"].front_default}
             alt={pokemon.name}
         />
 
+        <div className="contenedor">
+          {pokemonTypes.map(typeName => (
+            <div 
+              key={typeName} 
+              className="elementos"
+              style={{ backgroundColor: typeColors[typeName] }}
+            >
+              <img 
+                src={`https://raw.githubusercontent.com/duiker101/pokemon-type-svg-icons/master/icons/${typeName}.svg`} 
+                alt={typeName} 
+              />
+            </div>
+          ))}
+        </div>
+
+        {/* Tabla de estadísticas */}
         <div className="stats">
             {pokemon.stats.map((stat) => (
-            <div
-                key={stat.stat.name}
-                className="stat"
-            >
-                <span>
-                {stat.stat.name}
-                </span>
-
-                <div className="barContainer">
-                <div
-                    className="bar"
-                    style={{
-                    width: `${stat.base_stat}%`
-                    }}
-                ></div>
+            <div key={stat.stat.name} className="stat">
+                <span>{stat.stat.name}</span>
+                <div className="barra">
+                  <div
+                      className="bar"
+                      style={{ width: `${stat.base_stat}%` }}
+                  ></div>
                 </div>
-
-                <span>
-                {stat.base_stat}
-                </span>
+                <span>{stat.base_stat}</span>
             </div>
             ))}
         </div>
